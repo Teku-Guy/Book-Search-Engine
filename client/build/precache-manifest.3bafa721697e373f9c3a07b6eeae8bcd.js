8e4120b5c56a63d2ff9e337438bf23205a41eb55
Gustavo Muratalla <gusmuratalla@gmail.com>
2021-09-12 15:27:12 -0700

    trying to deploy

---
+++ client/build/precache-manifest.3bafa721697e373f9c3a07b6eeae8bcd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "947f3e1b25a3f9a82e9827ec0548a6f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfb6230903a0ef68ca46",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "a6095983442d1d5e9992",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "bfb6230903a0ef68ca46",
+    "url": "/static/js/2.95a8144c.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/static/js/2.95a8144c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6095983442d1d5e9992",
+    "url": "/static/js/main.1a0b559a.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
